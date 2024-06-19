37c01905a479cd05bf7b845ed0cb51379225e5c8
D'Angelo DeNiro <dangelo.l.deniro@gmail.com>
2024-06-19 14:02:52 -0600

    Fixed YARN/NPM Issue, Sourced Logo for Frontend, Created NavBar, Installed, Imported Bootstrap CSS

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# beespoke
+## beespoke
 
 ## Models
     Note: 
+
     Before any CLOTHING ITEM model instance is created, a SUPPLIER and ACCESSORY instance must be created.
+    To solve React-Scripts Error verify yarn installation via NODE JS
+
+
+
+## Todo: Review frontend functionality of the backend models, create inital migration for hard coded accessesories
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import BeespokeLogo from './BeespokeLogo.JPG';
 
 function App() {
   return (
     <div className="App">
+
+    <Nav />
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+{/*         <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={BeespokeLogo} className="Beespoke-logo-main" alt="logo"/>
+{/*         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
