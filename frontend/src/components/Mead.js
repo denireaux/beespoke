0c1343c4f9163bc4e613f2442004eb4f7e0b6c06
D'Angelo DeNiro <dangelo.l.deniro@gmail.com>
2024-07-10 16:54:47 -0600

    Created MeadItem Frontend component, need to create Mead.css

--- frontend/src/components/Mead.js
+++ frontend/src/components/Mead.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Mead = () => {
-  return (
-    <section className="et-slide" id="tab-mead">
-      <h1>Mead</h1>
-      <h3>Information about our mead products.</h3>
-    </section>
-  );
+
+
+    const [meadItems, setMeadItems] = useState([]);
+
+    const fetchMeadItems = async () => {
+        const url = 'http://localhost:8000/api/mead/';
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json();
+            setMeadItems(data);
+        } else {
+            console.error('Failed to fetch mead items');
+        }
+    };
+
+    useEffect(() => {
+        fetchMeadItems();
+    }, []);
+
+    console.log(meadItems)
+  
+    return (
+        <section className="et-slide" id="tab-mead">
+        <h1>Mead</h1>
+        <h3>Information about our mead products.</h3>
+        
+        {meadItems.map(item => (
+            <div key={item.id}>
+                <h2>{item.name}</h2>
+                <p>{item.description}</p>
+            </div>
+        ))}
+        </section>
+    );
 }
 
 export default Mead;
